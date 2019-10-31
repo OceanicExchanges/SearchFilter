70002ab1268d15eb363e40602524aba2086776a8
Moritz Knabben <me@mo42.de>
2019-10-31 10:51:09 +0100

    Move frontend to root

--- Frontend/config/index.js
+++ Frontend/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../../Backend/src/main/webapp/'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/SearchFilter/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
